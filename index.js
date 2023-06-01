71ad1fa0fa49ea75838714661361f9aad6d42440
文静 <ruhuiwen0608@gmail.com>
2023-06-01 16:47:05 +0300

    user management and add login router

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 const { connectToDatabase } = require("./util/db");
 const { PORT } = require("./util/config");
 const notesRouter = require("./controllers/notes");
-
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const middleware = require("./util/middleware");
 app.use(express.json());
 
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHander);
 
 const start = async () => {
   await connectToDatabase();
