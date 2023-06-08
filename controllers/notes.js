2d889243d04ebff6c7aabcfd4cf5136b8cfcbb29
文静 <ruhuiwen0608@gmail.com>
2023-06-08 13:08:29 +0300

    update req.query with search key word

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,27 +1,49 @@
 const router = require("express").Router();
 const { Note, User } = require("../models");
-
+const { Op } = require("sequelize");
 const noteFinder = async (req, res, next) => {
   req.note = await Note.findByPk(req.params.id);
   next();
 };
 
-//ref: https: sequelize.org/docs/v6/core-concepts/model-querying-basics/#specifying-attributes-for-select-queries
+// //ref: https: sequelize.org/docs/v6/core-concepts/model-querying-basics/#specifying-attributes-for-select-queries
+// router.get("/", async (req, res) => {
+//   if (req.query.important) {
+//     const notes = await Note.findAll({
+//       attributes: { exclude: ["userId"] },
+//       include: { model: User, attributes: ["name"] },
+//       where: { important: req.query.important === "true" },
+//     });
+//     res.json(notes);
+//   } else {
+//     const notes = await Note.findAll({
+//       attributes: { exclude: ["userId"] },
+//       include: { model: User, attributes: ["name"] },
+//     });
+//     res.json(notes);
+//   }
+// });
+
+//optional:
 router.get("/", async (req, res) => {
+  let whereObj = {};
+  // where.important = { [Op.in]: [true, false] };
+
   if (req.query.important) {
-    const notes = await Note.findAll({
-      attributes: { exclude: ["userId"] },
-      include: { model: User, attributes: ["name"] },
-      where: { important: req.query.important === "true" },
-    });
-    res.json(notes);
-  } else {
-    const notes = await Note.findAll({
-      attributes: { exclude: ["userId"] },
-      include: { model: User, attributes: ["name"] },
-    });
-    res.json(notes);
+    whereObj.important = req.query.important === "true";
   }
+  if (req.query.search) {
+    whereObj.content = { [Op.substring]: req.query.search };
+  }
+  const notes = await Note.findAll({
+    attributes: { exclude: ["userId"] },
+    include: {
+      model: User,
+      attributes: ["name"],
+    },
+    where: whereObj,
+  });
+  res.json(notes);
 });
 
 router.post("/", async (req, res) => {
