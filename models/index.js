c4a22b77412491904cfeaaca5df6ba4f92addb6b
文静 <ruhuiwen0608@gmail.com>
2023-06-14 15:21:11 +0300

    update database to many-to-many relationships

--- models/index.js
+++ models/index.js
@@ -1,12 +1,20 @@
 const Note = require("./note");
 const User = require("./user");
+const Team = require("./team");
+const Membership = require("./membership");
 
 User.hasMany(Note);
 Note.belongsTo(User);
+
+User.belongsToMany(Team, { through: Membership });
+Team.belongsToMany(User, { through: Membership });
+//https://sequelize.org/docs/v6/advanced-association-concepts/advanced-many-to-many/
 // Note.sync({ alter: true });
 // User.sync({ alter: true });
 
 module.exports = {
   Note,
   User,
+  Team,
+  Membership,
 };
