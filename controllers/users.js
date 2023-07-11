8c9b68e3a7767394dbdc2c87a5744bfe8f5bf029
文静 <ruhuiwen0608@gmail.com>
2023-07-11 11:03:54 +0300

    update api/users/:id route

--- controllers/users.js
+++ controllers/users.js
@@ -18,9 +18,14 @@ router.post("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const user = await User.findByPk(req.params.id);
+  const user = await User.findByPk(req.params.id, { include: { model: Note } });
   if (user) {
-    res.json(user);
+    // res.json(user);
+    res.json({
+      username: user.username,
+      name: user.name,
+      note_count: user.notes.length,
+    });
   } else {
     res.status(404).end();
   }
