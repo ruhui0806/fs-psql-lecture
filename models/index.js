e52f975f84bec4ed524b80f7941ae304b29bcb9d
文静 <ruhuiwen0608@gmail.com>
2023-07-11 11:38:07 +0300

    update models/index.js file to update with the new through table Users_notes

--- models/index.js
+++ models/index.js
@@ -2,12 +2,16 @@ const Note = require("./note");
 const User = require("./user");
 const Team = require("./team");
 const Membership = require("./membership");
+const UserNotes = require("./user_notes");
 
 User.hasMany(Note);
 Note.belongsTo(User);
 
 User.belongsToMany(Team, { through: Membership });
 Team.belongsToMany(User, { through: Membership });
+
+User.belongsToMany(Note, { through: UserNotes, as: "marked_notes" });
+Note.belongsToMany(User, { through: UserNotes, as: "users_marked" });
 //  https://sequelize.org/docs/v6/advanced-association-concepts/advanced-many-to-many/
 //  https://sequelize.org/docs/v6/core-concepts/assocs/#many-to-many-relationships
 // Note.sync({ alter: true });
@@ -18,4 +22,5 @@ module.exports = {
   User,
   Team,
   Membership,
+  UserNotes,
 };
